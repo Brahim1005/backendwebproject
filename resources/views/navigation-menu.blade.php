7bc04dbb988c2f77c44582ad1c677adb2063c7d8
Brahim1005 <brahim.harrar@student.ehb.be>
2023-12-27 15:55:45 +0100

    Changing logo's where needed

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,9 +5,9 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto" />
-                    </a>
+                <a href="/">
+                    <img width="74" src="{{asset('/logo/mademoiselle.jpg')}}" alt="">
+                </a>
                 </div>
 
                 <!-- Navigation Links -->
