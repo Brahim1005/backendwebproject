bf5824281cd4e1676013c046cae25e2644db73cd
Brahim1005 <brahim.harrar@student.ehb.be>
2024-01-14 01:49:40 +0100

    - Adding about page with list of sources and readme

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
 
-    <title>Contact Page</title>
+    <title>About Page</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<!--
+    <!--
 
 TemplateMo 546 Sixteen Clothing
 
@@ -26,9 +27,9 @@ https://templatemo.com/tm-546-sixteen-clothing
     <link rel="stylesheet" href="assets/css/templatemo-sixteen.css">
     <link rel="stylesheet" href="assets/css/owl.css">
 
-  </head>
+</head>
 
-  <body>
+<body>
 
     <!-- ***** Preloader Start ***** -->
     <div id="preloader">
@@ -37,91 +38,165 @@ https://templatemo.com/tm-546-sixteen-clothing
             <div></div>
             <div></div>
         </div>
-    </div>  
+    </div>
     <!-- ***** Preloader End ***** -->
 
     <!-- Header -->
     <header class="">
-      <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a class="navbar-brand" href="/"><h2>Made <em>moiselle</em></h2></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="/">Home
-                  <span class="sr-only">(current)</span>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                    <h2>Made <em>moiselle</em></h2>
                 </a>
-              </li> 
-              <li class="nav-item">
-                <a class="nav-link" href="{{url('faq')}}">FAQ Page</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{url('about')}}">About Us</a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="{{url('contactform')}}">Contact Us</a>
-              </li>
-              <li class="nav-item">              
-                @if (Route::has('login'))
-                  @auth
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Home
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('faq')}}">FAQ Page</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="{{url('about')}}">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('contactform')}}">Contact Us</a>
+                        </li>
+                        <li class="nav-item">
+                            @if (Route::has('login'))
+                            @auth
 
-                  <li class="nav-item">
-                    <a class="nav-link" href="{{url('showcart')}}"><i class="fas fa-shopping-cart"></i>Cart[{{$count}}]</a>
-                  </li>
-                    <x-app-layout>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('showcart')}}"><i
+                                    class="fas fa-shopping-cart"></i>Cart[{{$count}}]</a>
+                        </li>
+                        <x-app-layout>
 
-                    </x-app-layout>
-                  @else
-                    <li><a class="nav-link" href="{{ route('login') }}">Log in</a></li>
-                    @if (Route::has('register'))
-                    <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
-                    @endif
-                  @endauth
-                @endif
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+                        </x-app-layout>
+                        @else
+                        <li><a class="nav-link" href="{{ route('login') }}">Log in</a></li>
+                        @if (Route::has('register'))
+                        <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                        @endif
+                        @endauth
+                        @endif
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-      @if(session()->has('message'))
+        @if(session()->has('message'))
         <div class="alert alert-success">
-          <button type="button" class="close" data-dismiss="alert"></button>
-          {{session()->get('message')}}
+            <button type="button" class="close" data-dismiss="alert"></button>
+            {{session()->get('message')}}
         </div>
-      @endif
+        @endif
     </header>
 
     <!-- Page Content -->
     <div class="page-heading about-heading header-text">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="text-content">
-              <h4>about us</h4>
-              <h2>our company</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="text-content">
+                        <h4>about us</h4>
+                        <h2>Read Me</h2>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
 
-    
+    <h1 align="center" style="padding-top: 15px; font-weight:bold; font-size:x-large;">Short Information about the website</h1>
+    <br>
+    <br>
+
+    <h3 style="padding-left: 20px; font-weight:bold;">Algemene informatie</h3>
+    <p style="padding-left: 20px;">Ik heb gekozen om een website te maken voor een small business van mijn schoonzus. Ze heeft een winkel waar ze
+        jurken verkoopt. 
+        Ik heb dus een website gemaakt daarvoor.
+    </p>
+    <br>
+
+    <h3 style="padding-left: 20px; font-weight:bold;">Minimum Requirements</h3>
+    <p style="padding-left: 20px;">
+       -Login system :
+       <br>
+        - Users can log in ✅ <br>
+        - Visitors can create a new account ✅<br>
+        - Users may or may not be an administrator ✅<br>
+        - Only an administrator can promote another user to administrator status (or create a new user that is an admin) -> Can promote via DataBase by changing usertype ✅<br>
+        <br>
+       - Profile pagina : <br>
+        - Each user has their own profile page ✅ <br>
+        - A user can edit their own user data ✅ <br>
+        <br>
+       - Latest news : <br>
+        - Admins can add, edit, and delete news products ✅ <br>
+        - Every visitor of the website can view the news products ✅ <br>
+        - These news items have at least the following: <br>
+        - Title ✅<br>
+        - Cover image ✅<br>
+        - Content ✅<br>
+       - FAQ page : <br>
+       <br>
+        - There is a list of questions and answers ✅ <br>
+        - Admins can add, edit, and delete FAQ question and answer pairs ✅ <br>
+        - Every visitor of the website can view the FAQ page ✅ <br>
+        <br>
+       - Contact page : <br>
+        - Visitors can fill out a contact form ✅<br>
+        - The content of submitted forms will be sent to the administrators ✅ <br>
+    </p>
+    <br>
+
+    <h3 style="padding-left: 20px; font-weight:bold;">Extra Features</h3>
+    <p style="padding-left: 20px;">
+        - Shopping system (user have a cart and can add products in it) ✅ <br>
+        - In the contact page, users can see the real location of the store on Google Maps ✅ <br>
+        - You can search products via the searchbar ✅ <br>
+        - Users can pose questions that might be added to the FAQ ✅ <br>
+        - Admins can add answers to the posed FAQ questions through the admin panel ✅ <br>
+        - There is also a 2-factor authentification that is working (activable on profile user) ✅
+    </p>
+
+    <br>
+    <br>
+    <h3 style="padding-left: 20px; font-weight:bold;">List of sources</h3>
+    <p style="padding-left: 20px;">
+        Ik heb 2 templates gebruikt in dit project : Een template voor de design van de website en een template voor de admin panel 
+        <br>
+        Eerste template (Website) -> https://templatemo.com/tm-546-sixteen-clothing
+        <br>
+        Tweede template (Admin-panel) -> https://github.com/BootstrapDash/corona-free-dark-bootstrap-admin-template
+        <br>
+        Deze video heeft mij ook heel veel geholpen bij het integreren van deze templates -> https://youtu.be/gMzf49j9Qm0?si=YWGDJippCobq7Xks
+        <br>
+        <p style="padding-left: 20px; font-weight:bold;">Ter info: Deze templates zijn gewoon gebruikt om een design te geven!</p>
+        <br>
+    </p>
+
+
     <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="inner-content">
-              <p>Copyright &copy; 2024 Mademoiselle., Ltd.
-            
-            - Design: <a rel="nofollow noopener" href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="inner-content">
+                        <p>Copyright &copy; 2024 Mademoiselle., Ltd.
+
+                            - Design: <a rel="nofollow noopener" href="https://templatemo.com"
+                                target="_blank">TemplateMo</a></p>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </footer>
 
 
@@ -138,18 +213,18 @@ https://templatemo.com/tm-546-sixteen-clothing
     <script src="assets/js/accordions.js"></script>
 
 
-    <script language = "text/Javascript"> 
-      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
-      function clearField(t){                   //declaring the array outside of the
-      if(! cleared[t.id]){                      // function makes it static and global
-          cleared[t.id] = 1;  // you could use true and false, but that's more typing
-          t.value='';         // with more chance of typos
-          t.style.color='#fff';
-          }
-      }
+    <script language="text/Javascript">
+        cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+        function clearField(t) {                   //declaring the array outside of the
+            if (!cleared[t.id]) {                      // function makes it static and global
+                cleared[t.id] = 1;  // you could use true and false, but that's more typing
+                t.value = '';         // with more chance of typos
+                t.style.color = '#fff';
+            }
+        }
     </script>
 
 
-  </body>
+</body>
 
 </html>
